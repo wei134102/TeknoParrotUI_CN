0a56e857eb41c5c16cb06104032bed2f1f57501c
ReaverTeknoGods <c0de187nig@gmail.com>
2026-07-07 02:15:58 +0400

    Add missing resources and improve MultiGameButtonConfig

--- TeknoParrotUi/Properties/Resources.Designer.cs
+++ TeknoParrotUi/Properties/Resources.Designer.cs
@@ -4284,6 +4284,15 @@ namespace TeknoParrotUi.Properties {
         }
         
         /// <summary>
+        ///   Looks up a localized string similar to only {0} of {1} selected games support this input API.
+        /// </summary>
+        public static string MultiGameButtonConfigApiSupportCount {
+            get {
+                return ResourceManager.GetString("MultiGameButtonConfigApiSupportCount", resourceCulture);
+            }
+        }
+        
+        /// <summary>
         ///   查找类似 Apply Changes 的本地化字符串。
         /// </summary>
         public static string MultiGameButtonConfigApplyChanges {
@@ -4574,6 +4583,15 @@ namespace TeknoParrotUi.Properties {
         }
         
         /// <summary>
+        ///   Looks up a localized string similar to {0} game(s) skipped: selected Input API not supported.
+        /// </summary>
+        public static string MultiGameButtonConfigGamesSkippedUnsupportedApi {
+            get {
+                return ResourceManager.GetString("MultiGameButtonConfigGamesSkippedUnsupportedApi", resourceCulture);
+            }
+        }
+        
+        /// <summary>
         ///   查找类似 Load Error 的本地化字符串。
         /// </summary>
         public static string MultiGameButtonConfigLoadError {
@@ -4597,6 +4615,15 @@ namespace TeknoParrotUi.Properties {
         public static string MultiGameButtonConfigLoadFromProfileTooltip {
             get {
                 return ResourceManager.GetString("MultiGameButtonConfigLoadFromProfileTooltip", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Merged Input (All APIs).
+        /// </summary>
+        public static string MultiGameButtonConfigMergedInput {
+            get {
+                return ResourceManager.GetString("MultiGameButtonConfigMergedInput", resourceCulture);
             }
         }
         
@@ -5056,6 +5083,15 @@ namespace TeknoParrotUi.Properties {
         public static string MultiGameButtonConfigUnsavedChanges {
             get {
                 return ResourceManager.GetString("MultiGameButtonConfigUnsavedChanges", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to You have unsaved changes. Do you want to save them before switching input mode?.
+        /// </summary>
+        public static string MultiGameButtonConfigUnsavedChangesSwitchApi {
+            get {
+                return ResourceManager.GetString("MultiGameButtonConfigUnsavedChangesSwitchApi", resourceCulture);
             }
         }
         
